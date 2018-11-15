70f8930ecb4a7598c385ab2e2d475e828f95a172
CJMinecraft01 <speed2pc2@gmail.com>
2018-11-15 20:50:35 +0000

    Preparing for 1.12 release. Now need to backport

--- src/main/java/cjminecraft/core/items/ItemBlockMeta.java
+++ src/main/java/cjminecraft/core/items/ItemBlockMeta.java
@@ -1,36 +1,45 @@
 package cjminecraft.core.items;
 
-import java.util.List;
-
+import cjminecraft.core.blocks.BlockMeta;
 import cjminecraft.core.util.EnumUtils;
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.NonNullList;
-import net.minecraftforge.common.util.EnumHelper;
 
 /**
- * An item which uses metadata
+ * An item block which uses meta data. Used by {@link BlockMeta}
  * 
  * @author CJMinecraft
  *
  * @param <E>
  *            The enum which the item uses
  */
-public class ItemMeta<E extends Enum<E> & IStringSerializable> extends Item {
+public class ItemBlockMeta<E extends Enum<E> & IStringSerializable> extends ItemBlock {
 
 	private E[] values;
 
 	/**
-	 * Create a new meta item using the different values of the given enum class
+	 * Create a new meta item block using the different values of the given enum
+	 * class
 	 * 
 	 * @param enumClass
 	 *            The enum class containing the different meta values
+	 * @param block
+	 *            The block which the {@link ItemBlock} will use
 	 */
-	public ItemMeta(final Class<E> enumClass) {
+	public ItemBlockMeta(final Class<E> enumClass, Block block) {
+		super(block);
 		this.values = EnumUtils.getEnumValues(enumClass);
 		setHasSubtypes(true);
+		this.setMaxDamage(0);
+	}
+
+	@Override
+	public int getMetadata(int damage) {
+		return damage;
 	}
 
 	@Override
